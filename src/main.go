77c425219e3852fe9164eb33350ea6a770ce87d1
uhhhclem <rrossney@gmail.com>
2015-05-02 20:37:17 +0000

    Added input handler and clarified state design.

--- src/main.go
+++ src/main.go
@@ -3,13 +3,19 @@ package main
 import "hunters"
 
 func main() {
-    
-    g := &hunters.Game{}
-    g.State = &hunters.Start{}
-    for {
-        g.State = g.State.Handle()
-        if g.State == nil {
-            break
-        }
-    }
+
+	g := &hunters.Game{
+		Output: make(chan *hunters.Prompt),
+		Input:  make(chan string),
+	}
+	g.State = &hunters.Start{Game: g}
+	
+	go g.HandleInput()
+
+	for {
+		g.State = g.State.Handle()
+		if g.Done {
+			break
+		}
+	}
 }
